8b5e4519bf47c127afe7288373d79e2efa722dcd
Petr Kočí <petr.koci@gmail.com>
2014-01-15 19:14:50 +0100

    dynamická osa y

--- D3/barchart.js
+++ D3/barchart.js
@@ -4,7 +4,7 @@ var h = 480;
 var svg = d3.select("body")
     .append("svg")
     .attr("width", w)
-    .attr("height", h)
+    .attr("height", h);
 
 var popisky = [
     "Období",
@@ -17,10 +17,15 @@ var popisky = [
     "Celkem"
 ];
 
-
 /* load data */
 
 d3.csv("../data/marketing-revenue.csv", function(dataset) {
+
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(dataset, function(d, i) {
+            return d.celkem;
+        })])
+        .range([0, h]);
 
     /* insert elements */
     svg.selectAll("rect")
@@ -31,10 +36,13 @@ d3.csv("../data/marketing-revenue.csv", function(dataset) {
             return i * w / dataset.length;
         })
         .attr("y", function(d) {
-            return h - d.celkem / 20;
+            return h - yScale(d.celkem);
         })
-        .attr("width", w / dataset.length - 1)
-        .attr("height", function(d, i) {
-            return parseFloat(d.celkem / 20);
+        .attr("width", w / dataset.length - 30)
+        .attr("height", function(d) {
+            return yScale(d.celkem);
         })
+        .attr("fill", "skyblue")
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
 });
