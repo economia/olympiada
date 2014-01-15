fa9bb060465f1bd16d405388b3b91ae1a9d2b6cf
Petr Kočí <petr.koci@gmail.com>
2014-01-15 11:55:29 +0100

    D3 load data

---
+++ D3/barchart.js
@@ -0,0 +1,15 @@
+var popisky = [
+    "Období",
+    "Města",
+    "Vysílací práva",
+    "Globální reklamní partneři",
+    "Partneři v hostitelských zemích",
+    "Prodej vstupenek",
+    "Poplatky za použití olympijských motivů",
+    "Celkem"
+]
+
+d3.csv("../data/marketing-revenue.csv", function(d) {
+    console.log(d);
+
+});
