51d8c59cbb8763eda367937b896b4a79e8a1b7d8
Petr Kočí <petr.koci@gmail.com>
2014-01-15 12:39:34 +0100

    create svg

--- D3/barchart.js
+++ D3/barchart.js
@@ -1,3 +1,13 @@
+var data;
+
+var w = 670;
+var h = 480;
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+
 var popisky = [
     "Období",
     "Města",
@@ -7,9 +17,12 @@ var popisky = [
     "Prodej vstupenek",
     "Poplatky za použití olympijských motivů",
     "Celkem"
-]
+];
+
+/* load data */
 
 d3.csv("../data/marketing-revenue.csv", function(d) {
-    console.log(d);
-
+    data = d;
 });
+
+/* insert elements */
