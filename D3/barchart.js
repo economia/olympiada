eb06b6b61d696fd4b57416da021b44ce26805b2e
Petr Kočí <petr.koci@gmail.com>
2014-01-15 13:26:49 +0100

    základní barchart

--- D3/barchart.js
+++ D3/barchart.js
@@ -1,5 +1,3 @@
-var data;
-
 var w = 670;
 var h = 480;
 
@@ -19,10 +17,24 @@ var popisky = [
     "Celkem"
 ];
 
+
 /* load data */
 
-d3.csv("../data/marketing-revenue.csv", function(d) {
-    data = d;
-});
+d3.csv("../data/marketing-revenue.csv", function(dataset) {
 
-/* insert elements */
+    /* insert elements */
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+            return i * w / dataset.length;
+        })
+        .attr("y", function(d) {
+            return h - d.celkem / 20;
+        })
+        .attr("width", w / dataset.length - 1)
+        .attr("height", function(d, i) {
+            return parseFloat(d.celkem / 20);
+        })
+});
