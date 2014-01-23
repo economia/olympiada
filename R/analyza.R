25d6b9b641a9a8df07d4cb709fe7da4743e7cce2
Jan Bocek <honza.bocek@gmail.com>
2014-01-23 19:43:10 +0100

    analyza.R ted vycisti dataset

--- R/analyza.R
+++ R/analyza.R
@@ -1,4 +1,4 @@
-df <- read.csv(file="vyskavaha.csv", header=F, sep="|", quote = "")
+df <- read.csv(file="../data/vyskavaha.csv", header=F, sep="|", quote = "")
 
 colnames(df) <- c("jmeno", "prezdivka", "pohlavi", "vyska", "vaha", "narozeni", "umrti", "klub", "stat", "sport", 
             "hry1", "udalost1", "tym1", "poradi1", "medaile1", "detail1",
@@ -27,6 +27,46 @@ colnames(df) <- c("jmeno", "prezdivka", "pohlavi", "vyska", "vaha", "narozeni", 
             "hry24", "udalost24", "tym24", "poradi24") 
 
 df <- df[-1,]
+df <- df[-17069,]
+df <- df[-111970,]
+df <- df[-111968,]
+df <- df[-68637,]
+df <- df[-24529,]
+df <- df[-42019,]
+df <- df[-69805,]
+df <- df[-72794,]
+df <- df[-83990,]
+df <- df[-49884,]
+df <- df[-51601,]
+df <- df[-74496,]
+df <- df[-99459,]
+df <- df[-71578,]
+df <- df[-99323,]
+df <- df[-107983,]
+df <- df[-83459,]
+df <- df[-112387,]
+df <- df[-120936,]
+df <- df[-124733,]
+df <- df[-20039,]
+df <- df[-85951,]
+df <- df[-72790,]
+
+for(i in 11:50) {df[95711,i] <- df[95711,i+10]}
+df$hry1[95711] <- "1952 Summer"
+for(i in 11:50) {df[31294,i] <- df[31294,i+10]}
+df$hry1[31294] <- "1988 Summer"
+for(i in 11:90) {df[121162,i] <- df[121162,i+10]}
+df$hry1[121162] <- "1984 Winter"
+for(i in 11:50) {df[125001,i] <- df[125001,i+10]}
+df$hry1[125001] <- "1932 Summer"
+for(i in 11:50) {df[82327,i] <- df[82327,i+10]}
+df$hry1[82327] <- "1964 Summer"
+for(i in 11:60) {df[17454,i] <- df[17454,i+10]}
+df$hry1[17454] <- "1928 Summer"
+for(i in 11:90) {df[108919,i] <- df[108919,i+10]}
+df$hry1[108919] <- "1988 Summer"
+for(i in 11:50) {df[106212,i] <- df[106212,i+10]}
+df$hry1[106212] <- "1948 Summer"
 
 df$narozeni <- sub(".*</span> ", "", df$narozeni)
 df$narozeni <- sub(" in.*", "", df$narozeni)
@@ -41,6 +81,32 @@ df$vyska <- sub(" cm.*", "", df$vyska)
 df$vaha <- sub(".*lbs .", "", df$vaha)
 df$vaha <- sub(" kg.", "", df$vaha)
 df$vaha <- sub(" kg", "", df$vaha)
+
+# pojmenovat datové typy
+
+str(df)
+df$narozeni <- as.Date(df$narozeni)
+df$umrti <- as.Date(df$umrti)
+df$jmeno <- as.character(df$jmeno)
+df$prezdivka <- as.character(df$prezdivka)
+df$klub <- as.character(df$klub)
+df$stat <- as.character(df$stat)
+df$sport <- as.character(df$sport)
+df$vyska <- as.numeric(as.character((df$vyska)))
+df$vaha <- as.numeric(as.character((df$vaha)))
+df$pohlavi <- sub("Male", "T", df$pohlavi)
+df$pohlavi <- sub("Female", "F", df$pohlavi)
+df$pohlavi <- as.logical(df$pohlavi)
+df$stat <- sub("<br><span class=", "", df$stat)
+df$sport <- sub("Sport:</span> ", "", df$sport)
+df$hry1 <- sub("1956 Equestrian", "1956 Summer", df$hry1)
+
+levels(factor(df$hry1))
+df[df$hry1 == "John Benevides Souza (Sousa-)",]
+df[106212,]
+
+
+# samostatné soubory pro Česko/Československo
 
 df.cs <- df[df$stat=="Czechoslovakia",]
 df.cz <- df[df$stat=="Czech Republic",]
